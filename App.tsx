5b8c551fbf04cb1834a791c9da1bf1ec5e1ff62a
Tegan Rauh <onlysayinghi@gmail.com>
2020-10-18 20:52:19 -0500

    Change to function component

--- App.tsx
+++ App.tsx
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppLoading } from 'expo';
 import { Container } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import AppHeader from './src/AppHeader';
 
-export default class App extends React.Component {
-  state = {
-    isReady: false,
-  }
+const App = () => {
+  const [isReady, setIsReady] = useState(false);
 
-  async componentDidMount() {
-    await Font.loadAsync({
+  useEffect(() => {
+    Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       ...Ionicons.font,
+    }).then(() => {
+      setIsReady(true);
     });
-    this.setState({ isReady: true });
+  }, []);
+
+  if (!isReady) {
+    return <AppLoading />
   }
 
-  render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
+  return (
+    <Container>
+      <AppHeader />
+    </Container>
+  );
+};
 
-    return (
-      <Container>
-        <AppHeader />
-      </Container>
-    );
-  }
-}
+export default App;
